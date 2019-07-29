82514506b5fcf57bee9ba52fcbeb0aeec2c4334e
Angelo <valmocinajayangelo@gmail.com>
2019-07-29 18:24:51 +0800

    Added line item and cart models

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   resources :instruments
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  authenticated :user do 
-    root 'instruments#index'
-  end
-  
-  root to: redirect("/users/sign_in")
+
+  root 'instruments#index'
+  # authenticated :user do 
+  #   root 'instruments#index'
+  # end
+
+  # root to: redirect("/users/sign_in")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
